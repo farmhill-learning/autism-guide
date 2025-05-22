4a03bdfb569a4b513ae919bd20713fd0c2e6ca74
Anand Chitipothu <anandology@gmail.com>
2025-05-22 06:13:01 +0530

    fixed syntax error in bot.js

--- js/bot.js
+++ js/bot.js
@@ -1,13 +1,13 @@
-window.myScriptData = {{
+window.myScriptData = {
     clientId: '2ee3a95a1ded4fd59e957d548bc837a3',
     API_BASE_URL: 'https://api.chat.peepul.org/api/v2/'
-}};
+};
 
-(function(w,d,s,f,js,fjs){{
+(function(w,d,s,f,js,fjs){
     js=d.createElement(s);
     fjs=d.getElementsByTagName(s)[0];
     js.async=1;
     js.src=f;
     js.id='myWidgetScript';
     fjs.parentNode.insertBefore(js,fjs);
-}}(window,document,'script','https://api.chat.peepul.org/api/v2/popupbot/js'));
+}(window,document,'script','https://api.chat.peepul.org/api/v2/popupbot/js'));
